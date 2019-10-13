7913e29f846a98ef58ae07e93ebaac063dbe990e
answer-zf <feng18255163789@gmail.com>
2019-10-13 10:22:34 +0800

    router separation 1.0

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var router = require('./router')
 var app = express()
 
 app.engine('html', require('express-art-template'))
@@ -6,8 +7,8 @@ app.engine('html', require('express-art-template'))
 app.use('/public/', express.static('./public/'))
 app.use('/node_modules/', express.static('./node_modules/'))
 
+router(app)
+
 app.listen(3000, function() {
   console.log('node express is running')
 })
-
-module.exports = app
